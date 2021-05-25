4b713705255a199723020f15e9686bc8ad4e8722
Brenocsc <breno.csc.2013@hotmail.com.br>
2021-05-25 09:53:36 -0300

    update novo endereco

---
+++ novo-endereco/cadastrar-endereco.php
@@ -0,0 +1,26 @@
+<?php            
+  require "../conexaoMysql.php";
+  $pdo = mysqlConnect();
+
+  $cep = $logradouro = $cidade = $estado = "";
+
+  try {
+    $pdo->beginTransaction();
+
+    if (isset($_POST["cep"])) $cep = $_POST["cep"];
+    if (isset($_POST["logradouro"])) $logradouro = $_POST["logradouro"];
+    if (isset($_POST["cidade"])) $cidade = $_POST["cidade"];
+    if (isset($_POST["estado"])) $estado = $_POST["estado"];
+
+    $stmt = $pdo->prepare('INSERT INTO base_enderecos_ajax_clinica (cep, logradouro, cidade, estado) VALUES (?, ?, ?, ?)');
+    $stmt->execute([$cep, $logradouro, $cidade, $estado]);
+
+    // header("location: index.html");
+    // exit();
+  }
+  catch (Exception $e) {
+    // desfaz as operacoes em caso de erro (exceção lançada)
+    $pdo->rollBack();
+    exit('Falha na transação: ' . $e->getMessage());
+  }
+?>
