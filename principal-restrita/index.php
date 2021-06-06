728588ab4e967c19639f56623b9990b4c518caef
Brenocsc <breno.csc.2013@hotmail.com.br>
2021-06-06 14:50:27 -0300

    css fixes in private pages

--- principal-restrita/index.php
+++ principal-restrita/index.php
@@ -11,6 +11,7 @@
 
     <head>
         <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-CuOF+2SnTUfTwSZjCXf01h7uYhfOBuxIhGKPbfEJ3+FqH/s6cIFN9bGr1HmAg4fQ" crossorigin="anonymous">
         <link rel="stylesheet" href="main.css">
         <title>HxH - Início Restrita</title>
     </head>
